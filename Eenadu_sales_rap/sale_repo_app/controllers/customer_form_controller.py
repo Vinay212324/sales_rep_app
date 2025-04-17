c18c3509c7c907504318ea7133ad838e125fe51c
d seshikanth <depurisashi15@gmail.com>
2025-04-17 11:58:22 +0530

    for admin user

--- Eenadu_sales_rap/sale_repo_app/controllers/customer_form_controller.py
+++ Eenadu_sales_rap/sale_repo_app/controllers/customer_form_controller.py
@@ -519,3 +519,42 @@ class CustomerFormAPI(http.Controller):
         _logger.info(f"[CACHE] Data cached for {agent_login} with {len(result)} records.")
 
         return response
+
+
+
+
+    @http.route("/update/status", type="json", methods=['POST'], csrf=False, cors="*")
+    def _update_status(self,**params):
+
+        api_key = params.get('token')
+        if not api_key:
+            return {'success': False, 'message': 'Token is missing', 'code': "403"}
+
+        user = self._verify_api_key(api_key)
+        if not user:
+            return {'success': False, 'message': 'Invalid or expired token', 'code': "403"}
+
+        user_id = params.get("user_id")
+        if not user_id:
+            return {'error': 'User ID is required', "code": "403"}
+
+        try:
+            user_id = int(user_id)
+        except ValueError:
+            return {'error': 'Invalid User ID', "code": "403"}
+
+        user = request.env['res.users'].sudo().browse(user_id)
+        if not user.exists():
+            return {'error': 'User not found', "code": "403"}
+        if params.get("status") not in ["active","un_activ"]:
+            return {'error':'status is missing'}
+
+        user.write({
+            'status': params.get("status"),
+        })
+        if user.status == params.get("status"):
+            return {"success":"True","user_id":user.id, "code":"200"}
+        else:
+            return {"success":"False", "code":"403"}
+
+
